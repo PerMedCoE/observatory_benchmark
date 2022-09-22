12fddf5bb666188f2c066c5d2ea7d2fdb9f5052e
Tobias Duswald <tobias.duswald@cern.ch>
2022-09-22 19:04:53 +0200

    Add BDM diffusion for large unit test (#4)

---
+++ multiscale_benchmark/2022_09_hackathon/Biodynamo/unit_test_diffusion_1k/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script runs the diffusion unit test and moves the results to the
+# correct folder.
+
+# Retun if any command fails
+set -e
+
+# Check if BioDynaMo is sourced by checking if the environment variable
+# BDMSYS is set
+if [ -z "$BDMSYS" ]; then
+    echo "BioDynaMo is not sourced. Please source BioDynaMo first."
+    exit 1
+fi
+
+# Get the directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Go to the folder where the simulation is located
+cd $DIR/code_scripts/diffusion-large
+
+# If the directories `build` and `output` exist, delete them
+if [ -d "build" ]; then
+    rm -r build
+fi
+if [ -d "output" ]; then
+    rm -r output
+fi
+
+# Run the simulation
+bdm run
+
+# Move the results to the correct folder
+mv output/concentration_all.csv $DIR/results
+mv output/concentration_avg.csv $DIR/results
+mv output/diffusion-large/metadata $DIR/results
