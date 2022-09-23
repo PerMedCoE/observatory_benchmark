a1913192ebfffac4cdff00b8940a66ea65f55267
Tobias Duswald <tobias.duswald@cern.ch>
2022-09-23 18:35:49 +0200

    Fix format and extract statistics

--- multiscale_benchmark/2022_09_hackathon/Biodynamo/unit_test_cellcycle_stoch/code_scripts/src/cellcycle2.h
+++ multiscale_benchmark/2022_09_hackathon/Biodynamo/unit_test_cellcycle_stoch/code_scripts/src/cellcycle2.h
@@ -32,11 +32,11 @@ class MyCell : public Cell {  // our object extends the Cell object
     Base::Initialize(event);
 
     if (auto* mother = dynamic_cast<MyCell*>(event.existing_agent)) {
-        cycle_ = mother-> cycle_;
-        delt_ = mother-> delt_;
-        vmax_ = mother-> vmax_;
-        delt_ = mother->vinit_;
-        vrel_ = mother->vrel_;
+      cycle_ = mother->cycle_;
+      delt_ = mother->delt_;
+      vmax_ = mother->vmax_;
+      delt_ = mother->vinit_;
+      vrel_ = mother->vrel_;
       if (event.GetUid() == CellDivisionEvent::kUid) {
         // the daughter will be able to divide
         can_divide_ = true;
@@ -49,38 +49,37 @@ class MyCell : public Cell {  // our object extends the Cell object
   // getter and setter for division
   void SetCanDivide(bool d) { can_divide_ = d; }
   bool GetCanDivide() const { return can_divide_; }
-  
+
   // getter and setter for cells current phase of cell cycle
   void SetCycle(int c) { cycle_ = c; }
   int GetCycle() const { return cycle_; }
-  
+
   // getter and setter for cells maximum volume
   void SetVmax(double vm) { vmax_ = vm; }
   double GetVmax() const { return vmax_; }
-  
+
   // getter and setter for cells initial volume
   void SetVinit(double vi) { vinit_ = vi; }
   double GetVinit() const { return vinit_; }
-  
+
   // getter and setter for delt t
   void SetDelt(double dt) { delt_ = dt; }
   double GetDelt() const { return delt_; }
-  
+
   // getter and setter for relative cell volume
   void SetVrel(double vr) { vrel_ = vr; }
   double GetVrel() const { return vrel_; }
-  
 
  private:
   // declare new data member and define their type
   // private data can only be accessed by public function and not directly
   bool can_divide_;
-  int  cycle_;
-  double  vmax_;
+  int cycle_;
+  double vmax_;
   double vinit_;
   double delt_;
   double vrel_;
-};  
+};
 
 // Define growth behaviour
 struct Growth : public Behavior {
@@ -92,81 +91,84 @@ struct Growth : public Behavior {
   void Run(Agent* agent) override {
     if (auto* cell = dynamic_cast<MyCell*>(agent)) {
       auto* random = Simulation::GetActive()->GetRandom();
-      double ran = random->Uniform(0,1)*1.0;
-      double gaus = random->Gaus(0.9,1.0)*1.0;
-      
+      double ran = random->Uniform(0, 1) * 1.0;
+      double gaus = random->Gaus(0.9, 1.0) * 1.0;
+
       // Counter for Delta t at each stage
       // Used for calculating probability of moving to next state.
       cell->SetDelt(cell->GetDelt() + 0.1);
-      
-      // If statements for checking what states we are in and if 
+
+      // If statements for checking what states we are in and if
       // a cell moves to the next state based on cumulitave probability.
-     if (cell->GetCycle() == 1){
-       double p1 = (cell->GetDelt()/7) * cell->GetDelt();
-          if(p1 > ran){
-              // Changing cells state number or "cycle" postiion.
-              cell->SetCycle(2);
-              // Delta t is always reset when exiting a state for use in the next state.
-              cell->SetDelt(0);
+      if (cell->GetCycle() == 1) {
+        double p1 = (cell->GetDelt() / 7) * cell->GetDelt();
+        if (p1 > ran) {
+          // Changing cells state number or "cycle" postiion.
+          cell->SetCycle(2);
+          // Delta t is always reset when exiting a state for use in the next
+          // state.
+          cell->SetDelt(0);
         }
-          
-      } else if (cell->GetCycle() == 2){
-       double p2 = (cell->GetDelt()/6) * cell->GetDelt();
-         if(p2 > ran){
-              cell->SetCycle(3);
-              cell->SetDelt(0);
+
+      } else if (cell->GetCycle() == 2) {
+        double p2 = (cell->GetDelt() / 6) * cell->GetDelt();
+        if (p2 > ran) {
+          cell->SetCycle(3);
+          cell->SetDelt(0);
         }
-       
-      } else if (cell->GetCycle() == 3){
-       double p3 = (cell->GetDelt()/3) * cell->GetDelt();
-        if(p3 > ran){
-             cell->SetCycle(4);
-             cell->SetDelt(0);
+
+      } else if (cell->GetCycle() == 3) {
+        double p3 = (cell->GetDelt() / 3) * cell->GetDelt();
+        if (p3 > ran) {
+          cell->SetCycle(4);
+          cell->SetDelt(0);
         }
-       
+
       } else {
-       double p4 = (cell->GetDelt()/2) * cell->GetDelt();
-        if(p4 > ran){
-             cell->SetCycle(1);
-             cell->SetDelt(0);
-             // Checking if cell has reached the critical volume which leads to cell division.
-             // Here 0.975 Vmax is roughly 195% the initial cell volume.  
-            if (cell->GetVolume() > cell->GetVmax()*0.975){
-             cell->Divide();
-             }
+        double p4 = (cell->GetDelt() / 2) * cell->GetDelt();
+        if (p4 > ran) {
+          cell->SetCycle(1);
+          cell->SetDelt(0);
+          // Checking if cell has reached the critical volume which leads to
+          // cell division. Here 0.975 Vmax is roughly 195% the initial cell
+          // volume.
+          if (cell->GetVolume() > cell->GetVmax() * 0.975) {
+            cell->Divide();
+          }
         }
-       
       }
-          
-      // Checking if our cells volume is less than the maximum possible valuable achievalbe.
-      // if yes cell grows if no then cell does not grow.
+
+      // Checking if our cells volume is less than the maximum possible valuable
+      // achievalbe. if yes cell grows if no then cell does not grow.
       if (cell->GetVolume() < cell->GetVmax()) {
         double alpha = 1.0;
-        //cell->ChangeVolume(cell->GetVmax() - cell->GetVolume());
-        cell->ChangeVolume(alpha * cell->GetVolume()*((cell->GetVmax() - cell->GetVolume())/cell->GetVmax())*gaus);
+        // cell->ChangeVolume(cell->GetVmax() - cell->GetVolume());
+        cell->ChangeVolume(
+            alpha * cell->GetVolume() *
+            ((cell->GetVmax() - cell->GetVolume()) / cell->GetVmax()) * gaus);
       }
-      
-      //cell->SetVrel((cell->GetVolume()/cell->GetVinit())*100.0);
+
+      // cell->SetVrel((cell->GetVolume()/cell->GetVinit())*100.0);
     }
   }
 };
-    
+
 inline int Simulate(int argc, const char** argv) {
-    
-// Adding space edge of but to be used in larger use case.    
+  // Adding space edge of but to be used in larger use case.
   /*auto set_param = [](Param* param) {
     param->bound_space = Param::BoundSpaceMode::kOpen;
     param->min_bound = 0;
     param->max_bound = 60;  // cube of 100*100*100
-  }; */   
-    
+  }; */
+
   // Create a new simulation
-  Simulation simulation(argc, argv);
+  auto set_param = [&](Param* param) { param->statistics = true; };
+  Simulation simulation(argc, argv, set_param);
   auto* rm = simulation.GetResourceManager();
-  auto *scheduler = simulation.GetScheduler();  // Get the Scheduler
+  auto* scheduler = simulation.GetScheduler();  // Get the Scheduler
 
   size_t nb_of_cells = 1;  // number of cells in the simulation
-  
+
   for (size_t i = 0; i < nb_of_cells; ++i) {
     MyCell* cell = new MyCell({30, 30, 30});
     cell->SetDiameter(10.0);
@@ -182,11 +184,12 @@ inline int Simulate(int argc, const char** argv) {
   }
 
   // Run simulation for one timestep
-  // time steps are in tenths of an hour for a total of 48 hours equivalent in time steps.
+  // time steps are in tenths of an hour for a total of 48 hours equivalent in
+  // time steps.
   simulation.GetScheduler()->Simulate(480);
 
   scheduler->PrintInfo(std::cout);
-  
+
   std::cout << "Simulation completed successfully!" << std::endl;
   return 0;
 }
