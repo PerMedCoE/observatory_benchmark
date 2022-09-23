3988f65403f9ff87398cc56a90d33163d0b269e1
Tobias Duswald <tobias.duswald@cern.ch>
2022-09-23 15:15:56 +0200

    Add run script for cell cycle

---
+++ multiscale_benchmark/2022_09_hackathon/Biodynamo/unit_test_cellcycle/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script runs the cell cycle test and moves the results to the
+# correct folder.
+
+# Retun if any command fails
+set -e
+
+# Check if BioDynaMo is sourced by checking if the environment variable
+# BDMSYS is set
+if [ -z "$BDMSYS" ]; then
+    echo "BioDynaMo is not sourced. Please source BioDynaMo first."
+    exit 1
+fi
+
+# Get the directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Go to the folder where the simulation is located
+cd $DIR/code_scripts
+
+# If the directories `build` and `output` exist, delete them
+if [ -d "build" ]; then
+    rm -r build
+fi
+if [ -d "output" ]; then
+    rm -r output
+fi
+
+# Run the simulation
+bdm run
+
+# Move the results to the correct folder
+mv output/cellcycle1/metadata $DIR/results
