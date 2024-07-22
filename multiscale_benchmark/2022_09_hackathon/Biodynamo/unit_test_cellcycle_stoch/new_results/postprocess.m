61377d78063a657e9365e5aee50669b9d04c1492
Vasileios Vavourakis <vasvav@gmail.com>
2024-07-22 17:56:13 +0300

    revised also the MATLAB code to visualize the new results

--- multiscale_benchmark/2022_09_hackathon/Biodynamo/unit_test_cellcycle_stoch/new_results/postprocess.m
+++ multiscale_benchmark/2022_09_hackathon/Biodynamo/unit_test_cellcycle_stoch/new_results/postprocess.m
@@ -2,23 +2,37 @@ clear
 clc
 close all
 
-data = table2array(readtable('cell-0-sim1.csv'));
-
-Time = data(:,1);
-Volume = data(:,2);
-Phase = data(:,3);
-Age = data(:,4);
-
-Volume_min = Volume(1);
-
 figure, hold on
-plot(Time, Volume/Volume_min, '-r')
+for I = 1 : 9
+    fname = sprintf('output%d/cell-0.csv',I);
+    data = table2array(readtable(fname));
+
+    Time = data(:,1);
+    Volume = data(:,2);
+    Phase = data(:,3);
+    Age = data(:,4);
+    
+    Volume_min = Volume(1);
+
+    plot(Time, Volume/Volume_min, '-')
+end
 xlabel('time (hours)'), ylabel('cell volume (normalized to minimum value)')
 grid on
 hold off
 
 figure, hold on
-plot(Time, Phase, '-b')
+for I = 1 : 9
+    fname = sprintf('output%d/cell-0.csv',I);
+    data = table2array(readtable(fname));
+
+    Time = data(:,1);
+    Volume = data(:,2);
+    Phase = data(:,3);
+    Age = data(:,4);
+
+    plot(Time, Phase, '-')
+end
 xlabel('time (hours)'), ylabel('cell phase (1:G0/G1; 2:S; 3:G2; 4:M)')
 grid on
 hold off
+
